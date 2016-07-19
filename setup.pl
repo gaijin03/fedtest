0b0c551577fc363e81702ea4b3d0446d498cbb51
Brian Christiansen <brian@schedmd.com>
2016-07-19 17:02:16 -0600

    Uncomment option to make gdb work
    Some regression tests use ptrace and it won't work unless gdb can work.

--- setup.pl
+++ setup.pl
@@ -197,7 +197,7 @@ for (1..$NUM_CLUSTERS) {
 			   "-e MANPATH=/slurm/$cname/share/man " . #set MANPATH env variable
 			   "-e $testsuite_env " .	#set testsuite env variable
 			   "-t " .			#allocate a tty.
-			   #"--security-opt='seccomp=unconfined' " . #allows gdb to work
+			   "--security-opt='seccomp=unconfined' " . #allows gdb to work
 			   "$DOCKER_IMAGE " .		#docker image
 			   "tail -f /dev/null");	#keep container running
 
