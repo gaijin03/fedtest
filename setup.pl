9f6057de03f8ba786ca46138d7aaf33c0c21c0f8
Brian Christiansen <brian@schedmd.com>
2016-09-06 15:30:54 -0600

    Add #

--- setup.pl
+++ setup.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 #######################################################################
 # Written by Brian Christiansen <brian@schedmd.com>
-
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
